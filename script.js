56040634a09df49f9082e61763ab6332a2932c85
Itisha Rana <itisharana2008@gmail.com>
2025-11-28 15:32:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const API_KEY = "edc8e4a9d0cd46b9803162142252711";
+
+async function getWeather() {
+    const city = document.getElementById("cityInput").value;
+    const resultCard = document.getElementById("result");
+    const errorBox = document.getElementById("error");
+
+    if (!city) {
+        errorBox.textContent = "⚠ Please enter a city name!";
+        resultCard.classList.add("hidden");
+        return;
+    }
+
+    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) throw new Error("City not found");
+
+        const data = await response.json();
+        errorBox.textContent = "";
+        resultCard.classList.remove("hidden");
+
+        document.getElementById("location").textContent = `${data.location.name}, ${data.location.country}`;
+        document.getElementById("icon").src = data.current.condition.icon;
+        document.getElementById("temp").textContent = `${data.current.temp_c}°C`;
+        document.getElementById("condition").textContent = data.current.condition.text;
+        document.getElementById("humidity").textContent = data.current.humidity;
+        document.getElementById("wind").textContent = data.current.wind_kph;
+
+    } catch (error) {
+        resultCard.classList.add("hidden");
+        errorBox.textContent = "❌ City not found. Try again!";
+    }
+}
